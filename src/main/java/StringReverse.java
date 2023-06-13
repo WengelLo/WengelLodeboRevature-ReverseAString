93fefb79e9d4e4586437a472ff6844764d70ca67
Wengel Lodebo <tes.wengel@gmail.com>
2023-06-13 01:21:00 +0000

    g

--- src/main/java/StringReverse.java
+++ src/main/java/StringReverse.java
@@ -13,6 +13,11 @@ public class StringReverse {
      * @return the reverse of str.
      */
     public String reverse(String str){
-        return null;
+        String reversedStr = "";
+        for(int i = 0; i< str.length(); i++){
+            reversedStr = str.charAt(i) + reversedStr;
+           
+        }
+        return reversedStr;
     }
 }
